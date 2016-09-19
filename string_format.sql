543c2011307423aca87cb6ec61a4b06cfa426a00
Damon Dudek <billdudek_007@hotmail.com>
2016-09-19 15:09:54 -0300

    Update string_format.sql
    Ajustado a função para que possa utilizar o identificador do valor em mais de um trecho do texto. Exemplo.
    
    String: Data de hoje [{0}/{0}/{1}]
    Format: 09, 2016
    Result:  Data de hoje [09/09/2016]

--- string_format.sql
+++ string_format.sql
@@ -2,15 +2,20 @@ create or replace function Fu_String_Format(p_String in varchar2
                                            ,p_Format in varchar2) return varchar2 is
 
   v_Retorno varchar2(32767) := p_String;
+  v_Posicao pls_integer := 0;
 begin
 
-  for c_Cursor in (select Regexp_Substr(p_String, '{\d{1,}}', 1, level) as Identificador
-                         ,level as Posicao
-                     from Dual
-                   connect by level <= Length(Regexp_Replace((Regexp_Replace(p_String, '{\d{1,}}', 'ง')), '[^ง]'))
+  for c_Cursor in (select Posicao.Identificador
+                     from (select Regexp_Substr(p_String, '{\d{1,}}', 1, level) as Identificador
+                             from Dual
+                           connect by level <= Length(Regexp_Replace((Regexp_Replace(p_String, '{\d{1,}}', 'ยง')), '[^ยง]'))) Posicao
+                    group by Identificador
                     order by 1)
   loop
-    v_Retorno := replace(v_Retorno, c_Cursor.Identificador, trim(Substr(Regexp_Substr(',' || p_Format, ',[^,]*', 1, c_Cursor.Posicao), 2)));
+  
+    v_Posicao := v_Posicao + 1;
+    v_Retorno := replace(v_Retorno, c_Cursor.Identificador, trim(Substr(Regexp_Substr(',' || p_Format, ',[^,]*', 1, v_Posicao), 2)));
+  
   end loop;
 
   return v_Retorno;
