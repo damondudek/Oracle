9983fc1fa78c67e73f0c205ee2b4ffaeef756e6e
Damon Dudek <billdudek_007@hotmail.com>
2016-09-27 17:28:42 -0300

    Update string_format.sql

--- string_format.sql
+++ string_format.sql
@@ -5,11 +5,17 @@ create or replace function Fu_String_Format(p_String in varchar2
   v_Posicao   pls_integer := 0;
   v_Expressao varchar2(100) := '{\d{1,}}';
 begin
+  if p_String is null
+     or p_Format is null
+  then
+    return v_Retorno;
+  end if;
 
   for c_Cursor in (select Posicao.Identificador
                      from (select Regexp_Substr(p_String, v_Expressao, 1, level) as Identificador
                              from Dual
-                           connect by level <= Length(Regexp_Replace((Regexp_Replace(p_String, v_Expressao, Chr(198))), '[^' || Chr(198) || ']'))) Posicao
+                           connect by level <=
+                                      Length(Regexp_Replace((Regexp_Replace(p_String, v_Expressao, Chr(198))), '[^' || Chr(198) || ']'))) Posicao
                     group by Identificador
                     order by 1)
   loop
